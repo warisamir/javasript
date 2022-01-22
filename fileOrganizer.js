f50691a732f1f852a124a0ea3957f831a4d7c4fd
Waris Amir <warisamir1918@gmail.com>
2022-01-22 19:27:21 +0530

    push organiser

---
+++ fileOrganizer.js
@@ -0,0 +1,142 @@
+let inputArr = process.argv.slice(2);
+let fs = require("fs");
+let path = require("path");
+
+// console.log(inputArr);
+// node main.js tree "directoryPath"
+// node main.js organize "directoryPath"
+// node main.js help
+let command = inputArr[0];
+let types = {
+    media: ["mp4", "mkv",'mov','wmv','flv','avi','avchd','webm','webdl','mpeg','mpg','mp2','m4v','f4v','f4a','m4b','m4r'],
+    audio:["mp3","wav","wma","flac","aac","aiff","au","ogg","ec3","wproj","weba"],
+    image:['bmp','tif','eps','raw','cr2','nef','orf','srf','jpeg','jpg','png','gif'],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt',,"ppt","pptx", 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb","eps","dif","odt"]
+}
+switch (command) {
+    case "tree":
+        treeFn(inputArr[1]);
+        break;
+    case "organize":
+        organizeFn(inputArr[1]);
+        break;
+    case "help":
+        helpFn();
+        break;
+    default:
+        console.log("Please 🙏 Input Right command");
+        break;
+}
+
+function organizeFn(dirPath) {
+    // console.log("organize command implemnted for ", dirPath);
+    // 1. input -> directory path given
+    let destPath;
+    if (dirPath == undefined) {
+        destPath = process.cwd();
+        return;
+    } else {
+        let doesExist = fs.existsSync(dirPath);
+        if (doesExist) {
+
+            // 2. create -> organized_files -> directory
+            destPath = path.join(dirPath, "organized_files");
+            if (fs.existsSync(destPath) == false) {
+                fs.mkdirSync(destPath);
+            }
+
+        } else {
+
+            console.log("Kindly enter the correct path");
+            return;
+        }
+    }
+    organizeHelper(dirPath, destPath);
+    // 3. identify categories of all the files present in that input directory  ->
+}
+function organizeHelper(src, dest) {
+    // 3. identify categories of all the files present in that input directory  ->
+    let childNames = fs.readdirSync(src);
+    // console.log(childNames);
+    for (let i = 0; i < childNames.length; i++) {
+        let childAddress = path.join(src, childNames[i]);
+        let isFile = fs.lstatSync(childAddress).isFile();
+        if (isFile) {
+            // console.log(childNames[i]);
+            let category = getCategory(childNames[i]);
+            console.log(childNames[i], "belongs to --> ", category);
+            // 4. copy / cut  files to that organized directory inside of any of category folder 
+            sendFiles(childAddress, dest, category);
+        }
+    }
+}
+function sendFiles(srcFilePath, dest, category) {
+    // 
+    let categoryPath = path.join(dest, category);
+    if (fs.existsSync(categoryPath) == false) {
+        fs.mkdirSync(categoryPath);
+    }
+    let fileName = path.basename(srcFilePath);
+    let destFilePath = path.join(categoryPath, fileName);
+    fs.copyFileSync(srcFilePath, destFilePath);
+    fs.unlinkSync(srcFilePath);
+    console.log(fileName, "copied to ", category);
+
+}
+function getCategory(name) {
+    let ext = path.extname(name);
+    ext = ext.slice(1);
+    for (let type in types) {
+        let cTypeArray = types[type];
+        for (let i = 0; i < cTypeArray.length; i++) {
+            if (ext == cTypeArray[i]) {
+                return type;
+            }
+        }
+    }
+    return "others";
+}
+function helpFn() {
+    console.log(`
+    List of All the commands:
+                 node main.js tree "directoryPath"
+                node main.js organize "directoryPath"
+                node main.js help
+                `);
+}
+function treeFn(dirPath) {
+    // let destPath;
+    if (dirPath == undefined) {
+
+        treeHelper(process.cwd(), "");
+        return;
+    } else {
+        let doesExist = fs.existsSync(dirPath);
+        if (doesExist) {
+            treeHelper(dirPath, "");
+        } else {
+
+            console.log("Kindly enter the correct path");
+            return;
+        }
+    }
+}
+
+function treeHelper(dirPath, indent) {
+    // is file or folder
+    let isFile = fs.lstatSync(dirPath).isFile();
+    if (isFile == true) {
+        let fileName = path.basename(dirPath);
+        console.log(indent + "├──" + fileName);
+    } else {
+        let dirName = path.basename(dirPath)
+        console.log(indent + "└──" + dirName);
+        let childrens = fs.readdirSync(dirPath);
+        for (let i = 0; i < childrens.length; i++) {
+            let childPath = path.join(dirPath, childrens[i]);
+            treeHelper(childPath, indent + "\t");
+        }
+    }
+}
